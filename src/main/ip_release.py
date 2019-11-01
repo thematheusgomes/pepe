3ce6c3efb60f4e3d87444edc7a57b9cf4533c276
Thales Sperling <thalesvon@gmail.com>
2019-11-01 19:15:04 -0300

    refactor for better logic

--- src/main/ip_release.py
+++ src/main/ip_release.py
@@ -1,25 +1,76 @@
 import json
-from main.util import validate_json_with_schema, allow_ip_on_waf
+import boto3
+import os
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+from main.log import Logger
 
+#from main.util import validate_json_with_schema, allow_ip_on_waf
+ip_sets = os.environ['ip_sets']
 
-def handler(event, context):
+LOGGER = Logger()
+schema = {
+    "type": "object",
+    "required": ["action", "location", "ip"],
+    "properties" : {
+        "action" : {"type" : "string"},
+        "location" : {"type" : "string"},
+        "ip" : {"type" : "string"},
+    }
+}
+
+def ip_release_handler(event):
 
     if validate_json_with_schema(event):
-        changed_wafs = allow_ip_on_waf(event)['message']    
-        body = {
-            "message": changed_wafs,
-            "input": event
-        }
-
-        response = {
-            "statusCode": 200,
-            "body": json.dumps(body)
-        }
-
-        return response
+        changed_wafs = allow_ip_on_waf(event)    
+        return changed_wafs
     else:
-        response = {
-            "statusCode":400,
-            "message": "error with input JSON. Check logs for JSON Schema"
-        }
-        return response
+        return {'action': 'waf', 'message': 'Error with JSON'}
+
+def validate_json_with_schema(json_input):
+    try:
+        validate(instance=json_input,schema=schema)
+        LOGGER.info('JSON Input is valid')
+        return True
+    except ValidationError as exception:
+        LOGGER.error(exception)
+        return False
+
+def allow_ip_on_waf(event):
+    with open(ip_sets) as f:
+        ip_sets_json = json.load(f)
+        LOGGER.info('%s will perform changes on Global WAF', event['location'])
+        waf_global = boto3.client('waf')
+        global_token = waf_global.get_change_token()
+        waf_global.update_ip_set(
+            IPSetId=ip_sets_json[event['location']]['GlobalId'],
+            ChangeToken=global_token['ChangeToken'],
+            Updates=[
+                {
+                    'Action': 'INSERT',
+                    'IPSetDescriptor': {
+                        'Type': 'IPV4',
+                        'Value': event['ip']
+                    }
+                }
+            ]
+            )
+
+        if 'RegionalId' in ip_sets_json[event['location']]:
+            LOGGER.info('%s will perform changes on Regional WAF', event['location'])
+            waf_regional = boto3.client('waf-regional')
+            regional_token = waf_regional.get_change_token()
+            waf_regional.update_ip_set(
+                IPSetId=ip_sets_json[event['location']]['RegionalId'],
+                ChangeToken=regional_token['ChangeToken'],
+                Updates=[
+                    {
+                        'Action': 'INSERT',
+                        'IPSetDescriptor': {
+                            'Type': 'IPV4',
+                            'Value': event['ip']
+                        }
+                    }
+                ]
+                )
+        return {'action': 'waf', 'message': 'IP {} allowed on WAF'.format(event['ip'])}
--- src/main/util.py
+++ src/main/util.py
@@ -1,68 +1,50 @@
 import json
 import boto3
 import os
+import urllib
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 from main.log import Logger
 
-ip_sets = os.environ['ip_sets']
-aws_region = os.environ['AWS_DEFAULT_REGION']
-
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+SLACK_URL = os.environ["SLACK_URL"]
 LOGGER = Logger()
-schema = {
-    "type": "object",
-    "required": ["action", "location", "ip"],
-    "properties" : {
-        "action" : {"type" : "string"},
-        "location" : {"type" : "string"},
-        "ip" : {"type" : "string"},
+
+def response_builder(response_code, message):
+    body = {
+        "message": message,
     }
-}
 
-def validate_json_with_schema(json_input):
+    response = {
+        "statusCode": response_code,
+        "body": body
+    }
+    return response
+
+def send_slack_message(channel_id,message):
     try:
-        validate(instance=json_input,schema=schema)
-        LOGGER.info('JSON Input is valid')
-        return True
-    except ValidationError as exception:
-        LOGGER.error(exception)
-        return False
+        data = urllib.parse.urlencode(
+                (
+                    ("token", BOT_TOKEN),
+                    ("channel", channel_id),
+                    ("text", message)
+                )
+            )
+        data = data.encode("ascii")
 
-def allow_ip_on_waf(event):
-    with open(ip_sets) as f:
-        ip_sets_json = json.load(f)
-        LOGGER.info('%s will perform changes on Global WAF', event['location'])
-        waf_global = boto3.client('waf')
-        global_token = waf_global.get_change_token()
-        response = waf_global.update_ip_set(
-        IPSetId=ip_sets_json[event['location']]['GlobalId'],
-        ChangeToken=global_token['ChangeToken'],
-        Updates=[
-            {
-                'Action': 'INSERT',
-                'IPSetDescriptor': {
-                    'Type': 'IPV4',
-                    'Value': event['ip']
-                }
-            }
-        ]
+        request = urllib.request.Request(
+            SLACK_URL, 
+            data=data, 
+            method="POST"
         )
 
-        if 'RegionalId' in ip_sets_json[event['location']]:
-            LOGGER.info('%s will perform changes on Regional WAF', event['location'])
-            waf_regional = boto3.client('waf-regional', region_name=aws_region)
-            regional_token = waf_regional.get_change_token()
-            response = waf_regional.update_ip_set(
-            IPSetId=ip_sets_json[event['location']]['RegionalId'],
-            ChangeToken=regional_token['ChangeToken'],
-            Updates=[
-                {
-                    'Action': 'INSERT',
-                    'IPSetDescriptor': {
-                        'Type': 'IPV4',
-                        'Value': event['ip']
-                    }
-                }
-            ]
-            )
-        return {'action': 'waf', 'message': 'IP allowed'}
+        request.add_header(
+            "Content-Type", 
+            "application/x-www-form-urlencoded"
+        )
+
+        urllib.request.urlopen(request).read()
+        return True
+    except Exception as exception:
+        LOGGER.error(exception)
+        return False
