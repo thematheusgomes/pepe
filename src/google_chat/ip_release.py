8ea7b6cb11d2ca4c38e83d2e8f3c9b698b68acdf
Matheus Gomes <thematheusgomes@gmail.com>
2020-12-08 17:48:20 -0300

    fix return error message to the chat user and fix the regiona ipset env variable

--- src/google_chat/ip_release.py
+++ src/google_chat/ip_release.py
@@ -3,13 +3,13 @@ import boto3
 from log import Logger
 
 LOGGER = Logger()
-REGINAL_IPSET = os.getenv('REGINAL_IPSET')
+REGIONAL_IPSET = os.getenv('REGIONAL_IPSET')
 GLOBAL_IPSET = os.getenv('GLOBAL_IPSET')
 
 def ip_release_handler(publicIp, user_name):
-    allow_ip_on_global_ipset(publicIp, user_name)
-    allow_ip_on_regional_ipset(publicIp, user_name)
-    return f'{user_name}, your ip ({publicIp}) has been released and now you can access Agent Portal and Superset'
+    text = allow_ip_on_global_ipset(publicIp, user_name)
+    text = allow_ip_on_regional_ipset(publicIp, user_name)
+    return text
 
 
 def allow_ip_on_global_ipset(publicIp, user_name):
@@ -30,6 +30,7 @@ def allow_ip_on_global_ipset(publicIp, user_name):
                 }
             ]
         )
+        return f'{user_name}, your ip ({publicIp}) has been released and now you can access Agent Portal and Superset'
     except Exception as e:
         LOGGER.error(e)
         return f'Something went wrong when releasing your IP {user_name}, please contact your administrator'
@@ -40,7 +41,7 @@ def allow_ip_on_regional_ipset(publicIp, user_name):
         waf_regional = boto3.client('waf-regional')
         regional_token = waf_regional.get_change_token()
         waf_regional.update_ip_set(
-            IPSetId=REGINAL_IPSET,
+            IPSetId=REGIONAL_IPSET,
             ChangeToken=regional_token['ChangeToken'],
             Updates=[
                 {
@@ -52,6 +53,7 @@ def allow_ip_on_regional_ipset(publicIp, user_name):
                 }
             ]
         )
+        return f'{user_name}, your ip ({publicIp}) has been released and now you can access Agent Portal and Superset'
     except Exception as e:
         LOGGER.error(e)
         return f'Something went wrong when releasing your IP {user_name}, please contact your administrator'
