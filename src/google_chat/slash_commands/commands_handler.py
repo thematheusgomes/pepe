c7d99889b4808b438d210caae4d223be8eeae16b
Matheus Gomes <thematheusgomes@gmail.com>
2020-12-30 11:52:19 -0300

    rename the validate_commands function

--- src/google_chat/slash_commands/commands_handler.py
+++ src/google_chat/slash_commands/commands_handler.py
@@ -4,10 +4,10 @@ from google_chat.slash_commands.ip_release import ip_release_handler
 LOGGER = Logger()
 
 def slash_command_handler(event, user_name, user_email):
-    text = validate_commands(event, user_name, user_email)
+    text = select_command(event, user_name, user_email)
     return text
 
-def validate_commands(event, user_name, user_email):
+def select_command(event, user_name, user_email):
     commandId = event['message']['slashCommand']['commandId']
     commandName = event['message']['annotations'][0]['slashCommand']['commandName']
     args = event['message']['argumentText'].split(' ')
