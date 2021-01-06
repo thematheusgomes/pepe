f6ec03060cb49cfa7a525454ddedb6d8a67a91f2
Matheus Gomes <thematheusgomes@gmail.com>
2021-01-06 11:38:47 -0300

    add first message when the bot is added for direct messages

--- src/app.py
+++ src/app.py
@@ -19,10 +19,12 @@ def on_event():
     user_email = event['user']['email']
     user_name = event['user']['displayName'] if event['user']['displayName'] else 'Dear'
     LOGGER.info(f'Event: {json.dumps(event)}')
-    if event['type'] == 'ADDED_TO_SPACE' and 'singleUserBotDm' not in event['space'].keys():
+    if event['type'] == 'ADDED_TO_SPACE' and 'singleUserBotDm' in event['space'].keys():
+        text = f'Thanks for starting to chat with me {user_name}, I will be very happy to help you!\n\nThe commands I have available are:\n\n-> `/wafiprelease`\n-> `/sgiprelease`'
+    elif event['type'] == 'ADDED_TO_SPACE' and 'singleUserBotDm' not in event['space'].keys():
         text = 'Thanks for adding me to "%s"!' % (event['space']['displayName'] if event['space']['displayName'] else 'this chat')
     elif event['type'] == 'MESSAGE' and 'slashCommand' not in event['message'].keys():
-        text = f'Sorry {user_name}, I\'m currently accepting only slash commands!\n\n*Commands List:*\n\n> `/wafiprelease`\n> `/sgiprelease`'
+        text = f'Sorry {user_name}, I\'m currently accepting only slash commands!\n\n*Commands List:*\n\n-> `/wafiprelease`\n-> `/sgiprelease`'
     elif event['type'] == 'MESSAGE' and 'slashCommand' in event['message'].keys():
         text = select_command(event, user_name, user_email)
     else:
