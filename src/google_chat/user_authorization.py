457cbb07eda2af34631ea719ff75f6b3d19fd9aa
Matheus Gomes <thematheusgomes@gmail.com>
2021-01-14 11:11:14 -0300

    return after finding the authorized user

--- src/google_chat/user_authorization.py
+++ src/google_chat/user_authorization.py
@@ -6,13 +6,9 @@ LOGGER = Logger()
 USERS = json.loads(os.getenv('USERS'))
 
 def user_authorization(user_name, user_email, type):
-    flag = False
     for user in USERS['users'][type]:
         if user_name == user['name'] and user_email == user['email']:
-            flag = True
-    if flag:
-        LOGGER.info(f'Authorized {type} user: {json.dumps({"name": user_name, "email": user_email})}')
-        return flag
-    else:
-        LOGGER.error(f'Unauthorized {type} user: {json.dumps({"name": user_name, "email": user_email})}')
-        return flag
+            LOGGER.info(f'Authorized {type} user: {json.dumps({"name": user_name, "email": user_email})}')
+            return True
+    LOGGER.error(f'Unauthorized {type} user: {json.dumps({"name": user_name, "email": user_email})}')
+    return False
