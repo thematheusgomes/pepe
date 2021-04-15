8ac561cc2d02a16e10dcfd36dd299aaafbe8fbfb
Matheus Gomes <thematheusgomes@gmail.com>
2021-04-15 11:19:50 -0300

    small changes

--- src/app.py
+++ src/app.py
@@ -12,8 +12,7 @@ def handler(event, context):
     if 'headers' not in event and 'resource' in event and event['resource'] == 'cloudwatch events':
         message = select_command(event, event['user']['displayName'], event['user']['email'])
         return send_message_to_rooms(message)
-    message = bot_handler(event)
-    return response(message)
+    return response(bot_handler(event))
 
 def bot_handler(event):
     token = event['headers']['Authorization'].replace('Bearer ', '')
@@ -22,19 +21,18 @@ def bot_handler(event):
     user_email = event['user']['email']
     user_name = event['user']['displayName'] if event['user']['displayName'] else 'Dear'
     if event['type'] == 'ADDED_TO_SPACE' and 'singleUserBotDm' in event['space'].keys():
-        message = f'Thanks for starting to chat with me {user_name}, I will be very happy to help you!\n\nThe commands I have available are:\n\n-> `/iprelease`\n-> `/sgipupdate`'
         LOGGER.info(f'User {user_name} starts a conversation with Pepe')
+        return f'Thanks for starting to chat with me {user_name}, I will be very happy to help you!\n\nThe commands I have available are:\n\n-> `/iprelease`\n-> `/sgipupdate`'
     elif event['type'] == 'ADDED_TO_SPACE' and 'singleUserBotDm' not in event['space'].keys():
-        message = 'Thanks for adding me to "%s"!' % (event['space']['displayName'] if event['space']['displayName'] else 'this chat')
         LOGGER.info('Pepe was added to room "%s"' % (event['space']['displayName'] if event['space']['displayName'] else 'unknown'))
+        return 'Thanks for adding me to "%s"!' % (event['space']['displayName'] if event['space']['displayName'] else 'this chat')
     elif event['type'] == 'MESSAGE' and 'slashCommand' not in event['message'].keys():
-        message = f'Sorry {user_name}, I\'m currently accepting only slash commands!\n\n*Commands List:*\n\n-> `/iprelease`\n-> `/sgipupdate`\n\nIf the problem persists, try typing slash "/" then select the command you want from the command list that will appear.'
+        return f'Sorry {user_name}, I\'m currently accepting only slash commands!\n\n*Commands List:*\n\n-> `/iprelease`\n-> `/sgipupdate`\n\nIf the problem persists, try typing slash "/" then select the command you want from the command list that will appear.'
     elif event['type'] == 'MESSAGE' and 'slashCommand' in event['message'].keys():
-        message = select_command(event, user_name, user_email)
+        return select_command(event, user_name, user_email)
     else:
-        message = 'Invalid event type'
-        LOGGER.error(message)
-    return message
+        LOGGER.error('Invalid event type')
+        return 'Invalid event type'
 
 def response(message):
     LOGGER.info('Sending message to Google Chat')
