7a636f3b9710f17302d18ea6324818e4c6686ebd
Matheus Gomes <thematheusgomes@gmail.com>
2021-04-15 11:19:27 -0300

    add return message when user pass invalid ip or invalid argument number

--- src/bot/slash_commands/commands_handler.py
+++ src/bot/slash_commands/commands_handler.py
@@ -15,25 +15,26 @@ def select_command(event, user_name, user_email):
     if args[0] == '':
         del args[0]
     if commandId == '1':
-        message = waf_ip_release_handler(args, user_name, user_email)
-        return message
+        return waf_ip_release_handler(args, user_name, user_email)
     if commandId == '2':
-        message = sg_ip_release_handler(args, user_name, user_email)
-        return message
+        return sg_ip_release_handler(args, user_name, user_email)
 
 def waf_ip_release_handler(args, user_name, user_email):
-    if len(args) == 2 and args[0] == 'dynamic' and ip_address(args[1]).is_global:
-        return waf.dynamic_ip_handler(ip_version(args[1]), user_name, user_email)
-    elif len(args) == 2 and args[0] == 'fixed' and ip_address(args[1]).is_global:
-        return waf.fixed_ip_handler(ip_version(args[1]), user_name, user_email)
-    elif len(args) == 1 and args[0] == 'clean':
-        return waf.clean_ips_handler(user_name, user_email)
-    elif len(args) == 1 and args[0] == 'help':
-        return f'Hey {user_name},\n\nI\'ll show you the arguments you can use with the `iprelease` command:\n\n-> `/iprelease <publicIp>` (Add your ip to the dynamic ips list)\n-> `/iprelease dynamic <publicIp>` (Another option that add your ip to the dynamic ips list)\n-> `/iprelease fixed <publicIp>` (Add your ip to the fixed ips list)\n-> `/iprelease clean` (Remove all ips from dynamic ips list)\n\nTo find out what is your public ip access the link http://checkip.amazonaws.com'
-    elif len(args) == 1 and ip_address(args[0]).is_global:
-        return waf.dynamic_ip_handler(ip_version(args[0]), user_name, user_email)
-    else:
-        LOGGER.error(f'Invalid arguments: {args}')
+    try:
+        if len(args) == 2 and args[0] == 'dynamic' and ip_address(args[1]).is_global:
+            return waf.dynamic_ip_handler(ip_version(args[1]), user_name, user_email)
+        elif len(args) == 2 and args[0] == 'fixed' and ip_address(args[1]).is_global:
+            return waf.fixed_ip_handler(ip_version(args[1]), user_name, user_email)
+        elif len(args) == 1 and args[0] == 'clean':
+            return waf.clean_ips_handler(user_name, user_email)
+        elif len(args) == 1 and args[0] == 'help':
+            return f'Hey {user_name},\n\nI\'ll show you the arguments you can use with the `iprelease` command:\n\n-> `/iprelease <publicIp>` (Add your ip to the dynamic ips list)\n-> `/iprelease dynamic <publicIp>` (Another option that add your ip to the dynamic ips list)\n-> `/iprelease fixed <publicIp>` (Add your ip to the fixed ips list)\n-> `/iprelease clean` (Remove all ips from dynamic ips list)\n\nTo find out what is your public ip access the link http://checkip.amazonaws.com'
+        elif len(args) == 1 and ip_address(args[0]).is_global:
+            return waf.dynamic_ip_handler(ip_version(args[0]), user_name, user_email)
+        else:
+            return f'Hey {user_name},\n\nThis argument is invalid, currently I\'m accepting only the arguments below:\n\n-> `/sgipupdate help` (Shows a message similar to that with all accepted arguments for this command)\n-> `/iprelease <publicIp>` (Add your ip to the dynamic ips list)\n-> `/iprelease dynamic <publicIp>` (Another option that add your ip to the dynamic ips list)\n-> `/iprelease fixed <publicIp>` (Add your ip to the fixed ips list)\n-> `/iprelease clean` (Remove all ips from dynamic ips list)\n\nTo find out what is your public ip access the link http://checkip.amazonaws.com'
+    except Exception as e:
+        LOGGER.error(e)
         return f'Hey {user_name},\n\nThis argument is invalid, currently I\'m accepting only the arguments below:\n\n-> `/sgipupdate help` (Shows a message similar to that with all accepted arguments for this command)\n-> `/iprelease <publicIp>` (Add your ip to the dynamic ips list)\n-> `/iprelease dynamic <publicIp>` (Another option that add your ip to the dynamic ips list)\n-> `/iprelease fixed <publicIp>` (Add your ip to the fixed ips list)\n-> `/iprelease clean` (Remove all ips from dynamic ips list)\n\nTo find out what is your public ip access the link http://checkip.amazonaws.com'
 
 def sg_ip_release_handler(args, user_name, user_email):
