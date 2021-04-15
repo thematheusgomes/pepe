8d722d3f3bcb589c269c7e98fb69e953fb1b5960
Matheus Gomes <thematheusgomes@gmail.com>
2021-04-14 23:12:46 -0300

    improve exceeded arguments error

--- src/bot/slash_commands/commands_handler.py
+++ src/bot/slash_commands/commands_handler.py
@@ -20,26 +20,22 @@ def select_command(event, user_name, user_email):
         return message
 
 def waf_ip_release_handler(args, user_name, user_email):
-    if len(args) in range(3):
-        if args[0] == 'dynamic' and ip_address(args[1]).is_global:
-            return waf.dynamic_ip_handler(ip_version(args[1]), user_name, user_email)
-        elif args[0] == 'fixed' and ip_address(args[1]).is_global:
-            return waf.fixed_ip_handler(ip_version(args[1]), user_name, user_email)
-        elif args[0] == 'clean':
-            return waf.clean_ips_handler(user_name, user_email)
-        elif args[0] == 'help':
-            return f'Hey {user_name},\n\nI\'ll show you the arguments you can use with the `iprelease` command:\n\n-> `/iprelease <publicIp>` (Add your ip to the dynamic ips list)\n-> `/iprelease dynamic <publicIp>` (Another option that add your ip to the dynamic ips list)\n-> `/iprelease fixed <publicIp>` (Add your ip to the fixed ips list)\n-> `/iprelease clean` (Remove all ips from dynamic ips list)\n\nTo find out what is your public ip access the link http://checkip.amazonaws.com'
-        elif ip_address(args[0]).is_global:
-            return waf.dynamic_ip_handler(ip_version(args[0]), user_name, user_email)
-        else:
-            LOGGER.error(f'Invalid arguments: {args}')
-            return f'Hey {user_name},\n\nThis argument is invalid, currently I\'m accepting only the arguments below:\n\n-> `/sgipupdate help` (Shows a message similar to that with all accepted arguments for this command)\n-> `/iprelease <publicIp>` (Add your ip to the dynamic ips list)\n-> `/iprelease dynamic <publicIp>` (Another option that add your ip to the dynamic ips list)\n-> `/iprelease fixed <publicIp>` (Add your ip to the fixed ips list)\n-> `/iprelease clean` (Remove all ips from dynamic ips list)\n\nTo find out what is your public ip access the link http://checkip.amazonaws.com'
+    if len(args) == 2 and args[0] == 'dynamic' and ip_address(args[1]).is_global:
+        return waf.dynamic_ip_handler(ip_version(args[1]), user_name, user_email)
+    elif len(args) == 2 and args[0] == 'fixed' and ip_address(args[1]).is_global:
+        return waf.fixed_ip_handler(ip_version(args[1]), user_name, user_email)
+    elif len(args) == 1 and args[0] == 'clean':
+        return waf.clean_ips_handler(user_name, user_email)
+    elif len(args) == 1 and args[0] == 'help':
+        return f'Hey {user_name},\n\nI\'ll show you the arguments you can use with the `iprelease` command:\n\n-> `/iprelease <publicIp>` (Add your ip to the dynamic ips list)\n-> `/iprelease dynamic <publicIp>` (Another option that add your ip to the dynamic ips list)\n-> `/iprelease fixed <publicIp>` (Add your ip to the fixed ips list)\n-> `/iprelease clean` (Remove all ips from dynamic ips list)\n\nTo find out what is your public ip access the link http://checkip.amazonaws.com'
+    elif len(args) == 1 and ip_address(args[0]).is_global:
+        return waf.dynamic_ip_handler(ip_version(args[0]), user_name, user_email)
     else:
-        LOGGER.error(f'Exceeded the argument limit: {len(args)}')
-        return f'Hey {user_name},\n\nYou have exceeded the argument limit, for this command you can only put up to a maximum of 2 arguments.\n\nIf you want to understand a little better how it works, just type `/iprelease help`'
+        LOGGER.error(f'Invalid arguments: {args}')
+        return f'Hey {user_name},\n\nThis argument is invalid, currently I\'m accepting only the arguments below:\n\n-> `/sgipupdate help` (Shows a message similar to that with all accepted arguments for this command)\n-> `/iprelease <publicIp>` (Add your ip to the dynamic ips list)\n-> `/iprelease dynamic <publicIp>` (Another option that add your ip to the dynamic ips list)\n-> `/iprelease fixed <publicIp>` (Add your ip to the fixed ips list)\n-> `/iprelease clean` (Remove all ips from dynamic ips list)\n\nTo find out what is your public ip access the link http://checkip.amazonaws.com'
 
 def sg_ip_release_handler(args, user_name, user_email):
-    if args[0] == 'dev' or args[0] == 'qa' or args[0] == 'prod' or args[0] == 'tools' and ip_address(args[1]).is_global:
+    if len(args) == 2 and args[0] == 'dev' or args[0] == 'qa' or args[0] == 'prod' or args[0] == 'tools' and ip_address(args[1]).is_global:
         return security_group_handler(args[0], args[1], user_name, user_email)
     elif args[0] == 'help':
         return f'Hey {user_name},\n\nI\'ll show you the arguments you can use with the `sgipupdate` command:\n\n-> `/sgipupdate dev <publicIp>` (Update IP in the Dev Security Group)\n-> `/sgipupdate qa <publicIp>` (Update IP in the Qa Security Group)\n-> `/sgipupdate prod <publicIp>` (Update IP in the Prod Security Group)\n-> `/sgipupdate tools <publicIp>` (Update IP in the Tools Security Group)\n\nTo find out what is your public ip access the link http://checkip.amazonaws.com'
